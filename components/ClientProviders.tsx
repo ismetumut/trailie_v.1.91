b460308fcb78f6b99a0ee59f6170ef15fce773db
ismetumut <144053229+ismetumut@users.noreply.github.com>
2025-06-29 15:23:35 +0300

    Update ClientProviders.tsx

--- components/ClientProviders.tsx
+++ components/ClientProviders.tsx
@@ -11,17 +11,4 @@ export default function ClientProviders({ children }: { children: React.ReactNod
       </AuthProvider>
     </ThemeProvider>
   );
-'use client';
-
-import { ThemeProvider } from '@/components/theme-provider';
-import { AuthProvider } from '@/contexts/AuthContext';
-
-export default function ClientProviders({ children }: { children: React.ReactNode }) {
-  return (
-    <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
-      <AuthProvider>
-        {children}
-      </AuthProvider>
-    </ThemeProvider>
-  );
-} 
+}
